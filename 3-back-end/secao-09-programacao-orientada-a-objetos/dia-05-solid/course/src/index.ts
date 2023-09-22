c52f1ed5a792a5e440abd2d5838f3caac70288a5
Geovanna Otoni <geovannaotoni@gmail.com>
2023-09-22 18:09:05 -0300

    back-end 9.4

---
+++ 3-back-end/secao-09-programacao-orientada-a-objetos/dia-05-solid/course/src/index.ts
@@ -0,0 +1,35 @@
+import { ReadOnlyConnector, ReadOnlyRedisConnector } from "./Connectors";
+
+const token = 'ce42033d-9133-457a-a1a1-41ac0b63a333';
+const conn = new ReadOnlyRedisConnector({
+  host: 'redisdb',
+  port: 6379,
+});
+
+const main = async (connector: ReadOnlyConnector) => {
+  const count = await connector.getCount(token);
+  try {
+    console.log(count);
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+main(conn);
+
+main(conn);
+
+/*
+Saída:
+0
+3
+6
+9
+12
+15
+18
+21
+24
+27
+30
+*/
