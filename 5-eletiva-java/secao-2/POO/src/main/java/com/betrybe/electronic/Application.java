48ce8314a20abfe3932d7f3e6173d7910f9f2d89
Geovanna Otoni <geovannaotoni@gmail.com>
2023-11-20 16:33:46 -0300

    secao2

---
+++ 5-eletiva-java/secao-2/POO/src/main/java/com/betrybe/electronic/Application.java
@@ -0,0 +1,11 @@
+package com.betrybe.electronic;
+
+public class Application {
+  public static void main(String[] args) {
+    System.out.println("Iniciando sistema...");
+
+    Television television = new Television("Phillips","LCD", 32);
+
+    System.out.println("Finalizando sistema...");
+  }
+}
---
+++ 5-eletiva-java/secao-2/POO/src/main/java/com/betrybe/electronic/Television.java
@@ -0,0 +1,13 @@
+package com.betrybe.electronic;
+
+public class Television {
+  boolean isOn = false;
+  String brand;
+  String model;
+  int size;
+  public Television(String brand, String model, int size) {
+    this.brand = brand;
+    this.model = model;
+    this.size = size;
+  }
+}
